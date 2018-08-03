52314bc5e82fdeaa31d862a0e859cc6d93260074
Hao Luo <howlowck@gmail.com>
2018-08-03 11:18:09 -0500

    added travis build

--- src/tests/pizza.test.ts
+++ src/tests/pizza.test.ts
@@ -1,21 +1,4 @@
 /* global test */
-const initialWolfState = {
-  abilityCompleted: false,
-  activeAbility: '',
-  waitingFor: {
-    slotName: null,
-    turnCount: 0
-  },
-  messageQueue: [],
-  pendingData: {}
-}
-
-type PizzaData = {
-  pizzaType: string,
-  pizzaSize: string,
-  quantity: string  
-}
-
 describe('Add a Pizza a cart', () => { // Feature (ability)
  it('should run', () => {
    expect(true).toBeTruthy()
