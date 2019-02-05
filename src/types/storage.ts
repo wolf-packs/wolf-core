1aa9f854d22ae6413fd23b85a9a6ea25e08bd465
Kevin Leung <keleung@microsoft.com>
2019-02-05 14:58:11 -0600

    Updating storage overloads

--- src/types/storage.ts
+++ src/types/storage.ts
@@ -2,7 +2,7 @@ import { WolfState } from './state'
 import { Promiseable } from './generic'
 
 /**
- * Storage interface to persist wolf state. Since Wolf is stateless, the wolf state needs 
+ * Storage interface to persist the wolf state. Since Wolf is stateless, the wolf state needs 
  * to be persisted by the developer and so it can be utilized on the next turn. Wolf will handle
  * reading and saving the wolf state within the runner but the developer is required to provide
  * the persistence implementation.
@@ -10,14 +10,13 @@ import { Promiseable } from './generic'
  * _Developer should utilize this interface when implementing the persistence layer._
  */
 export interface WolfStateStorage extends StorageLayer<WolfState, void> {
-  readWolfState: () => Promiseable<WolfState>,
-  saveWolfState: (wolfState: WolfState) => Promiseable<void>,
+  read: () => Promiseable<WolfState>,
+  save: (wolfState: WolfState) => Promiseable<void>,
 }
 
 /**
- * Storage interface to assist developers in persisting their state. These read and save
- * functions will be made available to the developer within user defined ability functions
- * to help promote immutable patterns.
+ * Storage interface to assist developers in persisting their data. These read and save
+ * functions will be made available to the developer within the user defined ability functions.
  * 
  * _This interface should be implemented by the developer and passed into Wolf._
  */
