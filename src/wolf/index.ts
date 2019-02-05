8762f94e519ca62bb5adba4ee0bb54d6dda4b285
Kevin Leung <keleung@microsoft.com>
2019-02-05 15:46:29 -0600

    Updating API to consume storage layer interfaces

--- src/wolf/index.ts
+++ src/wolf/index.ts
@@ -56,24 +56,15 @@ export const makeWolfStoreCreator = (
  * The main Wolf function that will execute all wolf stages and yield the recommended next move
  * in the conversation flow. Wolf run is stateless but requires the following parameters..
  * 
- * @example 
- * // user conversation state interface
- * interface userConvoState {
- *  firstName: string,
- *  lastName: string
- * }
- * 
- * export * as wolf from 'wolf-core'
- * const results = wolf.run<userConvoState>(convoState, userMessageData, getAbilitiesFunc, defaultAbility)
- * 
- * @param conversationState Conversation persistent state per conversation session
+ * @param wolfStorage Wolf state storage layer. Wolf will handle saving wolf state by invoking the save()
+ * @param convoStorage User conversation state storage layer. Read/save will be made available
+ * to user functions in slot and abilities
  * @param userMessageData Natural Language Processing result
  * @param getAbilitiesFunc Abilities defined for the bot
  * @param defaultAbility Ability that will be used as the fallback if no ability is determined from the userMessageData
  * @param storeCreator Optional redux store creator, can be used with redux dev tools
  * @param getSlotDataFunc Optional getter function to retrieve slot data
- * @returns WolfState getter function, conversationState getter function and
- * OuttakeResult containing an array of output messages
+ * @returns Wolf's result containing an array of output messages
  */
 export const run = async <T extends object>(
   wolfStorage: WolfStateStorage,
@@ -84,16 +75,21 @@ export const run = async <T extends object>(
   storeCreator?: (wolfStateFromConvoState: { [key: string]: any } | null) => Store<WolfState>,
   getSlotDataFunc?: (setSlotFuncs: SetSlotDataFunctions) => Promiseable<IncomingSlotData[]>
 ) => {
+  // invoke user async functions
+  const [wolfState, convoState, nlpResult, abilities] = await Promise.all([
+    wolfStorage.read(),
+    convoStorage.read(),
+    userMessageData(),
+    getAbilitiesFunc()
+  ])
+
   // If user provides storeCreator param, invoke the redux store creator with the persisted wolfState (if available)
   // By defualt, redux store creator is invoked with the available wolfState
   // In either case, if wolfState is null, the storeCreator will instantiate a new defualt Wolf State
   const wolfStore: WolfStore = storeCreator ? storeCreator(wolfState) : makeWolfStoreCreator()(wolfState)
 
-  // Run Wolf Stages
-  const nlpResult: NlpResult = await userMessageData()
-  const abilities: Ability[] = await getAbilitiesFunc()
-  const convoState: T = conversationState
   const incomingSlotData: IncomingSlotData[] = getSlotDataFunc ?
+    // Run Wolf Stages
     await getSlotDataFunc({
       setSlotValue: (abilityName, slotName, value) => {
         wolfStore.dispatch(fillSlotAction(slotName, abilityName, value))
@@ -123,11 +119,8 @@ export const run = async <T extends object>(
     messages.forEach(addMessage)
   }
 
-  const wolfResult = outtake(wolfStore)
+  // Save wolf state by invoking user defined save function
+  await wolfStorage.save(wolfStore.getState())
 
-  return {
-    retrieveWolfState: () => wolfStore.getState(),
-    retrieveConversationState: () => convoState,
-    wolfResult
-  }
+  return outtake(wolfStore)
 }
