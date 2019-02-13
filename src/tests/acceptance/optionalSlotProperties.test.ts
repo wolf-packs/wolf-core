7d45907277f6877fd2118ec5cbb3143a1786f933
Kevin Leung <keleung@microsoft.com>
2019-02-13 12:16:51 -0600

    dynamic testing harness in progress..

--- src/tests/acceptance/optionalSlotProperties.test.ts
+++ src/tests/acceptance/optionalSlotProperties.test.ts
@@ -16,9 +16,77 @@ const defaultStore: UserConvoState = {
 const wolfStorage: wolf.WolfStateStorage = createStorage(getInitialWolfState())
 const convoStorage = createStorage(defaultStore)
 
+// const conversationTurns = [
+//   {
+//     input: { message: 'hello', entities: [], intent: 'magicWord' },
+//     expected: {
+//       message: ['Please name an animal... if you want.'],
+//       state: { 'animalName': null, 'magicWordStrict': null, 'magicWordStrict2': null }
+//     }
+//   },
+//   {
+//     input: { message: 'hippo', entities: [], intent: 'magicWord' },
+//     expected: {
+//       message: ['Please say \'wolf\'... not negotiable.'],
+//       state: { 'animalName': null, 'magicWordStrict': null, 'magicWordStrict2': null }
+//     }
+//   },
+//   {
+//     input: { message: 'hippo', entities: [], intent: 'magicWord' },
+//     expected: {
+//       message: ['Please follow directions.'],
+//       state: { 'animalName': null, 'magicWordStrict': null, 'magicWordStrict2': null }
+//     }
+//   },
+//   {
+//     input: { message: 'wolf', entities: [], intent: 'magicWord' },
+//     expected: {
+//       message: ['Please say \'wolf\' one more time.'],
+//       state: { 'animalName': null, 'magicWordStrict': null, 'magicWordStrict2': null }
+//     }
+//   },
+//   {
+//     input: { message: 'hippo', entities: [], intent: 'magicWord' },
+//     expected: {
+//       message: [
+//         'Please follow directions.',
+//         'You must say \'wolf\' a second time'
+//       ],
+//       state: { 'animalName': null, 'magicWordStrict': null, 'magicWordStrict2': null }
+//     }
+//   },
+//   {
+//     input: { message: 'wolf', entities: [], intent: 'magicWord' },
+//     expected: {
+//       message: [
+//         'Thank you for saying wolf wolf!',
+//         'You said: \'hippo\', \'wolf\', \'wolf\'!'
+//       ],
+//       state: {
+//         'animalName': 'hippo',
+//         'magicWordStrict': 'wolf',
+//         'magicWordStrict2': 'wolf'
+//       }
+//     }
+//   }
+// ]
+
 describe('Optional Slot Properties', () => { // Feature (ability)
   test('Optional Slot Properties', async () => {
 
+    // conversationTurns.forEach(async (turn) => {
+    //   const outputResult = await wolf.run(
+    //     wolfStorage,
+    //     convoStorage,
+    //     () => (turn.input),
+    //     () => abilities,
+    //     'magicWord'
+    //   )
+
+    //   expect(outputResult.messageStringArray).toEqual(turn.expected.message)
+    //   expect(convoStorage.read()).toEqual(turn.expected.state)
+
+    // })
     const outputResult = await wolf.run(
       wolfStorage,
       convoStorage,
