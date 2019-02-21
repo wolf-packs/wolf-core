2b7d2042ff222b0f1c764c5690b9c06db901c6d5
Dan Balma <dbalma@microsoft.com>
2019-02-21 15:17:48 -0600

    Update "Greet" tests to use runTest approach to match other tests

--- src/tests/endToEnd/greet.test.ts
+++ src/tests/endToEnd/greet.test.ts
@@ -1,65 +1,89 @@
 // /* global test */
 import * as wolf from '../..'
-import { getInitialWolfState, createStorage, StorageLayerType } from '../helpers'
-import greetAbility from './testAbilities/greetAbility'
-import { UserConvoState } from './testAbilities/greetAbility'
+import { getInitialWolfState, createStorage, StorageLayerType, TestCase, runTest } from '../helpers'
 
-const abilities: wolf.Ability<UserConvoState, StorageLayerType<UserConvoState>>[] = [
-  greetAbility
-]
+interface UserConvoState {
+  name: string | null,
+  age: string | null
+}
 
 const defaultStore: UserConvoState = {
-  name: null
+  name: null,
+  age: null
 }
+
+const abilities: wolf.Ability<UserConvoState, StorageLayerType<UserConvoState>>[] = [{
+  name: 'greet',
+  slots: [
+    {
+      name: 'name',
+      query: () => 'What is your name?'
+    },
+    {
+      name: 'age',
+      query: () => 'What is your age?',
+      retry: () => 'You must be older than 5.',
+      validate: (submittedValue: any) => {
+        const num = parseInt(submittedValue, 10);
+        if (num < 6) {
+          return { isValid: false, reason: 'too young' }
+        }
+        return { isValid: true, reason: null }
+      }
+    }
+  ],
+  onComplete: (convoStorageLayer, submittedData: any) => {
+    const currState = convoStorageLayer.read()
+    const newState = {
+      name: submittedData.name,
+      age: submittedData.age,
+    }
+    convoStorageLayer.save(newState)
+    return `Hello ${submittedData.name} who is ${submittedData.age}!`
+  }
+}] as wolf.Ability<UserConvoState, StorageLayerType<UserConvoState>>[]
 
 const wolfStorage: wolf.WolfStateStorage = createStorage(getInitialWolfState())
 const convoStorage = createStorage(defaultStore)
 
-describe('Greet', () => { // Feature (ability)
-  test('Basic Greet Flow', async () => {
+const greetTestCase: TestCase<UserConvoState, StorageLayerType<UserConvoState>> = {
+  description: 'Greet',
+  abilities: abilities,
+  defaultAbility: 'greet',
+  wolfStorage,
+  convoStorage,
+  conversationTurns: [
+    {
+      input: { message: 'hi', entities: [], intent: 'greet' },
+      expected: {
+        message: ['What is your name?'],
+        state: { name: null, age: null }
+      }
+    },
+    {
+      input: { message: 'Hao', entities: [], intent: null },
+      expected: {
+        message: ['What is your age?'],
+        state: { name: null, age: null }
+      }
+    },
+    {
+      input: { message: '3', entities: [], intent: null },
+      expected: {
+        message: ['too young', 'You must be older than 5.'],
+        state: { name: null, age: null }
+      }
+    },
+    {
+      input: { message: '30', entities: [], intent: null },
+      expected: {
+        message: ['Hello Hao who is 30!'],
+        state: { name: 'Hao', age: '30' }
+      }
+    }
+  ]
+}
 
-    const outputResult = await wolf.run(
-      wolfStorage,
-      convoStorage,
-      () => ({ message: 'hi', entities: [], intent: 'greet' }),
-      () => abilities,
-      'greet'
-    )
-
-    expect(outputResult.messageStringArray).toEqual(['What is your name?'])
-    expect(convoStorage.read()).toEqual({ 'name': null })
-
-    const outputResult2 = await wolf.run(
-      wolfStorage,
-      convoStorage,
-      () => ({ message: 'Hao', entities: [], intent: null }),
-      () => abilities,
-      'greet'
-    )
-
-    expect(outputResult2.messageStringArray).toEqual(['What is your age?'])
-    expect(convoStorage.read()).toEqual({ 'name': null })
-
-    const outputResult3 = await wolf.run(
-      wolfStorage,
-      convoStorage,
-      () => ({ message: '3', entities: [], intent: null }),
-      () => abilities,
-      'greet'
-    )
-
-    expect(outputResult3.messageStringArray).toEqual(['too young', 'You must be older than 5.'])
-    expect(convoStorage.read()).toEqual({ 'name': null })
-
-    const outputResult4 = await wolf.run(
-      wolfStorage,
-      convoStorage,
-      () => ({ message: '30', entities: [], intent: null }),
-      () => abilities,
-      'greet'
-    )
-
-    expect(outputResult4.messageStringArray).toEqual(['Hello Hao who is 30!'])
-    expect(convoStorage.read()).toEqual({ name: 'Hao' })
-  })
+describe('Basic Greet Flow Test', () => {
+  runTest(test, greetTestCase)
 })
