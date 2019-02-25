ab81d1e83392005ded367d4d5230d2a44a66aa26
Kevin Leung <keleung@microsoft.com>
2019-02-25 16:43:30 -0600

    wolf runner takes in array, only accessing first element for messageData

--- src/stages/intake.ts
+++ src/stages/intake.ts
@@ -7,7 +7,7 @@ const log = require('debug')('wolf:s1')
 /**
  * Intake Stage (S1):
  * 
- * Takes in user generated NlpResult object and creates messageData object, stored to state.
+ * Takes in user generated NlpResult object and creates messageDataArr based on incoming NlpResults, stored to state.
  * 
  * @param nlpResult user generated NLP Object
  * 
@@ -15,18 +15,38 @@ const log = require('debug')('wolf:s1')
  */
 export default function intake(
   { dispatch, getState }: Store,
-  nlpResult: NlpResult,
+  nlpResults: NlpResult[],
   incomingSlotData: IncomingSlotData[],
   defaultAbility: string | null = null
 ): void {
   logState(getState())
   dispatch(startIntakeStage())
-  // MessageData derived from user nlpResult
-  const messageData: MessageData = {
-    rawText: nlpResult.message,
-    intent: nlpResult.intent,
-    entities: nlpResult.entities
+
+  // MessageData derived from user nlpResults
+
+  let messageData: MessageData = { // default empty MessageData object
+    rawText: '',
+    intent: null,
+    entities: []
   }
+
+  // if at least one nlpResult is present, capture first element data
+  if (nlpResults.length > 0) {
+    messageData = {
+      rawText: nlpResults[0].message,
+      intent: nlpResults[0].intent,
+      entities: nlpResults[0].entities
+    }
+  }
+
+  // // MessageDataArr derived from user nlpResults
+  // const messageDataArr: MessageData[] = nlpResults.map(nlpResult => {
+  //   return {
+  //     rawText: nlpResult.message,
+  //     intent: nlpResult.intent,
+  //     entities: nlpResult.entities
+  //   }
+  // })
 
   // Write defaultAbility to state
   dispatch(setDefaultAbility(defaultAbility))
