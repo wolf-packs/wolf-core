58bdeefbd02a50e9d475c9c62d1708c67174635e
Gabrielle Crevecoeur <gacreve@microsoft.com>
2019-03-01 10:04:19 -0600

    updated abilities and slots for alarmBot

--- examples/simpleBots/alarmBot/abilities.ts
+++ examples/simpleBots/alarmBot/abilities.ts
@@ -1,93 +1,93 @@
-import { randomElement } from '../../../src/helpers'
 import { Ability, AllSyncStorageLayer } from '../../../src/types'
-
 export type StorageLayerType<T> = AllSyncStorageLayer<T>
 
 interface Alarm {
-  alarmName: string,
-  alarmTime: string
+    alarmName: string,
+    alarmTime: string
 }
 
 export interface UserState {
     alarms: Alarm[]
-  }
+}
 
 export default [
-  {
-    name: 'addAlarm',
-    traces: [],
-    onComplete: (submittedData, convoStorageLayer) => {
-      return new Promise((resolve, reject) => {
-        const convoState = convoStorageLayer.read()
-        const value = submittedData
-        const prevAlarms = convoState.alarms || []
-        const newState = {
-          alarms: [
-            ...prevAlarms,
-            value
-          ]
+    {
+        name: 'addAlarm',
+        traces: [{
+            slotName: 'alarmName'
+        },
+        {
+            slotName: 'alarmTime'
+        }],
+        onComplete: (submittedData, convoStorageLayer) => {
+            return new Promise((resolve, reject) => {
+                const convoState = convoStorageLayer.read()
+                const value = submittedData
+                const prevAlarms = convoState.alarms || []
+                const newState = {
+                    alarms: [
+                        ...prevAlarms,
+                        value
+                    ]
+                }
+
+                convoStorageLayer.save(newState)
+
+                setTimeout(() => {
+                    resolve(`Your ${value.alarmName} is added!`)
+                }, 500)
+            })
         }
+    },
+    {
+        name: 'removeAlarm',
+        traces: [
+            {
+                slotName: 'alarmName'
+            }
+        ],
+        onComplete: (submittedData, convoStorageLayer) => {
+            const { alarmName } = submittedData
+            const convoState = convoStorageLayer.read()
+            const stateAlarms = convoState.alarms || []
 
-        convoStorageLayer.save(newState)
+            // Check if alarm name exists
+            if (!stateAlarms.some((alarm: Alarm) => alarm.alarmName === alarmName)) {
+                return `There is no alarm with name ${alarmName}.`
+            }
 
-        setTimeout(() => {
-          resolve(`Your ${value.alarmName} is added!`)
-        }, 500)
-      })
-    }
-  },
-  {
-    name: 'removeAlarm',
-    slots: [
-      {
-        name: 'alarmName',
-        query: () => {
-          return 'What is the name of the alarm you would like to remove?'
+            // Remove alarm
+            const newAlarms = stateAlarms.filter((alarm: Alarm) => alarm.alarmName !== alarmName)
+            const newState = {
+                alarms: newAlarms
+            }
+
+            convoStorageLayer.save(newState)
+
+            return `The ${alarmName} has been removed.`
         }
-      }
-    ],
-    onComplete: (submittedData, convoStorageLayer ) => {
-      const { alarmName } = submittedData
-      const convoState = convoStorageLayer.read()
-      const stateAlarms = convoState.alarms || []
+    },
+    {
+        name: 'listAlarms',
+        traces: [],
+        onComplete: (submittedData, convoStorageLayer) => {
+            const convoState = convoStorageLayer.read()
+            const alarms = convoState.alarms || []
 
-      // Check if alarm name exists
-      if (!stateAlarms.some((alarm: Alarm) => alarm.alarmName === alarmName)) {
-        return `There is no alarm with name ${alarmName}.`
-      }
-
-      // Remove alarm
-      const newAlarms = stateAlarms.filter((alarm: Alarm) => alarm.alarmName !== alarmName)
-      const newState = {
-        alarms: newAlarms
-      }
-      
-      convoStorageLayer.save(newState)
-
-      return `The ${alarmName} has been removed.`
+            if (alarms.length === 0) {
+                return `You do not have any alarms!`
+            }
+            return alarms.map((alarms: Alarm) => alarms.alarmName + ' at ' + alarms.alarmTime).join(', ')
+        }
+    },
+    {
+        name: 'listAbility',
+        traces: [],
+        onComplete: (submittedData, convoStorageLayer, { getAbilityList }) => {
+            const abilityList = getAbilityList()
+            const abilities = abilityList.map((ability) => ability.name).join(', ')
+            const message = `Here are my abilities: ${abilities}`
+            return message
+        }
     }
-  },
-  {
-    name: 'listAlarms',
-    slots: [],
-    onComplete: (submittedData, convoStorageLayer) => {
-      const convoState = convoStorageLayer.read()
-      const alarms = convoState.alarms || []
-
-      if (alarms.length === 0) {
-        return `You do not have any alarms!`
-      }
-      return alarms.map((alarms: Alarm) => alarms.alarmName + ' at ' + alarms.alarmTime).join(', ')
-    }
-  },
-  {
-    name: 'listAbility',
-    slots: [],
-    onComplete: (submittedData, convoStorageLayer, { getAbilityList }) => {
-      const abilityList = getAbilityList()
-      const abilities = abilityList.map((ability) => ability.name).join(', ')
-      const message = `Here are my abilities: ${abilities}`
-      return message
-    }
-  }
 ] as Ability<UserState, StorageLayerType<UserState>>[]
