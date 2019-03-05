809aa8105bc08932cd775f02e19445c05b0a9b31
Kevin Leung <keleung@microsoft.com>
2019-03-05 15:05:59 -0600

    fixing formatting

--- examples/simpleBots/alarmBot/abilities.ts
+++ examples/simpleBots/alarmBot/abilities.ts
@@ -2,92 +2,92 @@ import { Ability, AllSyncStorageLayer } from '../../../src/types'
 export type StorageLayerType<T> = AllSyncStorageLayer<T>
 
 interface Alarm {
-    alarmName: string,
-    alarmTime: string
+  alarmName: string,
+  alarmTime: string
 }
 
 export interface UserState {
-    alarms: Alarm[]
+  alarms: Alarm[]
 }
 
 export default [
-    {
-        name: 'addAlarm',
-        traces: [{
-            slotName: 'alarmName'
-        },
-        {
-            slotName: 'alarmTime'
-        }],
-        onComplete: (submittedData, convoStorageLayer) => {
-            return new Promise((resolve, reject) => {
-                const convoState = convoStorageLayer.read()
-                const value = submittedData
-                const prevAlarms = convoState.alarms || []
-                const newState = {
-                    alarms: [
-                        ...prevAlarms,
-                        value
-                    ]
-                }
-
-                convoStorageLayer.save(newState)
-
-                setTimeout(() => {
-                    resolve(`Your ${value.alarmName} is added!`)
-                }, 500)
-            })
-        }
+  {
+    name: 'addAlarm',
+    traces: [{
+      slotName: 'alarmName'
     },
     {
-        name: 'removeAlarm',
-        traces: [
-            {
-                slotName: 'alarmName'
-            }
-        ],
-        onComplete: (submittedData, convoStorageLayer) => {
-            const { alarmName } = submittedData
-            const convoState = convoStorageLayer.read()
-            const stateAlarms = convoState.alarms || []
-
-            // Check if alarm name exists
-            if (!stateAlarms.some((alarm: Alarm) => alarm.alarmName === alarmName)) {
-                return `There is no alarm with name ${alarmName}.`
-            }
-
-            // Remove alarm
-            const newAlarms = stateAlarms.filter((alarm: Alarm) => alarm.alarmName !== alarmName)
-            const newState = {
-                alarms: newAlarms
-            }
-
-            convoStorageLayer.save(newState)
-
-            return `The ${alarmName} has been removed.`
+      slotName: 'alarmTime'
+    }],
+    onComplete: (submittedData, convoStorageLayer) => {
+      return new Promise((resolve, reject) => {
+        const convoState = convoStorageLayer.read()
+        const value = submittedData
+        const prevAlarms = convoState.alarms || []
+        const newState = {
+          alarms: [
+            ...prevAlarms,
+            value
+          ]
         }
-    },
-    {
-        name: 'listAlarms',
-        traces: [],
-        onComplete: (submittedData, convoStorageLayer) => {
-            const convoState = convoStorageLayer.read()
-            const alarms = convoState.alarms || []
 
-            if (alarms.length === 0) {
-                return `You do not have any alarms!`
-            }
-            return alarms.map((alarms: Alarm) => alarms.alarmName + ' at ' + alarms.alarmTime).join(', ')
-        }
-    },
-    {
-        name: 'listAbility',
-        traces: [],
-        onComplete: (submittedData, convoStorageLayer, { getAbilityList }) => {
-            const abilityList = getAbilityList()
-            const abilities = abilityList.map((ability) => ability.name).join(', ')
-            const message = `Here are my abilities: ${abilities}`
-            return message
-        }
+        convoStorageLayer.save(newState)
+
+        setTimeout(() => {
+          resolve(`Your ${value.alarmName} is added!`)
+        }, 500)
+      })
     }
+  },
+  {
+    name: 'removeAlarm',
+    traces: [
+      {
+        slotName: 'alarmName'
+      }
+    ],
+    onComplete: (submittedData, convoStorageLayer) => {
+      const { alarmName } = submittedData
+      const convoState = convoStorageLayer.read()
+      const stateAlarms = convoState.alarms || []
+
+      // Check if alarm name exists
+      if (!stateAlarms.some((alarm: Alarm) => alarm.alarmName === alarmName)) {
+        return `There is no alarm with name ${alarmName}.`
+      }
+
+      // Remove alarm
+      const newAlarms = stateAlarms.filter((alarm: Alarm) => alarm.alarmName !== alarmName)
+      const newState = {
+        alarms: newAlarms
+      }
+
+      convoStorageLayer.save(newState)
+
+      return `The ${alarmName} has been removed.`
+    }
+  },
+  {
+    name: 'listAlarms',
+    traces: [],
+    onComplete: (submittedData, convoStorageLayer) => {
+      const convoState = convoStorageLayer.read()
+      const alarms = convoState.alarms || []
+
+      if (alarms.length === 0) {
+        return `You do not have any alarms!`
+      }
+      return alarms.map((alarms: Alarm) => alarms.alarmName + ' at ' + alarms.alarmTime).join(', ')
+    }
+  },
+  {
+    name: 'listAbility',
+    traces: [],
+    onComplete: (submittedData, convoStorageLayer, { getAbilityList }) => {
+      const abilityList = getAbilityList()
+      const abilities = abilityList.map((ability) => ability.name).join(', ')
+      const message = `Here are my abilities: ${abilities}`
+      return message
+    }
+  }
 ] as Ability<UserState, StorageLayerType<UserState>>[]
--- examples/simpleBots/alarmBot/slots.ts
+++ examples/simpleBots/alarmBot/slots.ts
@@ -5,42 +5,42 @@ import { UserState } from './abilities'
 export type StorageLayerType<T> = AllSyncStorageLayer<T>
 
 export default [
-    {
-      name: 'alarmName',
-      query: () => { return 'What is the name of the alarm?' },
-      retry: (convoState, submittedData, turnCount) => {
-        const phrase = [`Please try a new name (attempt: ${turnCount})`, `Try harder.. (attempt: ${turnCount})`]
-        if (turnCount > phrase.length - 1) {
-          return phrase[phrase.length - 1]
-        }
-        return phrase[turnCount]
-      },
-      validate: (value) => {
-        if (value.toLowerCase() === 'hao') {
-          return { isValid: false, reason: `${value} is not a good alarm name.` }
-        }
-        return { isValid: true, reason: null }
-      },
-      onFill: (value) => `ok! name is set to ${value}.`
+  {
+    name: 'alarmName',
+    query: () => { return 'What is the name of the alarm?' },
+    retry: (convoState, submittedData, turnCount) => {
+      const phrase = [`Please try a new name (attempt: ${turnCount})`, `Try harder.. (attempt: ${turnCount})`]
+      if (turnCount > phrase.length - 1) {
+        return phrase[phrase.length - 1]
+      }
+      return phrase[turnCount]
     },
-    {
-      name: 'alarmTime',
-      query: () => { return 'What is the time you want to set?' },
-      retry: (convoState, submittedData, turnCount) => {
-        const phrases: string[] = ['let\'s try again', 'what is the time you want to set?']
-        return randomElement(phrases)
-      },
-      validate: (value: string) => {
-        if (!value.toLowerCase().endsWith('pm') && !value.toLowerCase().endsWith('am')) {
-          return {
-            isValid: false,
-            reason: 'Needs to set PM or AM',
-          }
-        }
+    validate: (value) => {
+      if (value.toLowerCase() === 'hao') {
+        return { isValid: false, reason: `${value} is not a good alarm name.` }
+      }
+      return { isValid: true, reason: null }
+    },
+    onFill: (value) => `ok! name is set to ${value}.`
+  },
+  {
+    name: 'alarmTime',
+    query: () => { return 'What is the time you want to set?' },
+    retry: (convoState, submittedData, turnCount) => {
+      const phrases: string[] = ['let\'s try again', 'what is the time you want to set?']
+      return randomElement(phrases)
+    },
+    validate: (value: string) => {
+      if (!value.toLowerCase().endsWith('pm') && !value.toLowerCase().endsWith('am')) {
         return {
-          isValid: true
+          isValid: false,
+          reason: 'Needs to set PM or AM',
         }
-      },
-      onFill: (value) => `ok! time is set to ${value}.`
-    }
-  ] as Slot<StorageLayerType<UserState>>[]
+      }
+      return {
+        isValid: true
+      }
+    },
+    onFill: (value) => `ok! time is set to ${value}.`
+  }
+] as Slot<StorageLayerType<UserState>>[]
